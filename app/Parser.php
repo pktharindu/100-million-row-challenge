6683628ac8b189647074fd4481151406650ceb8c
P. K. Tharindu <pktharindu@outlook.com>
2026-02-27 09:31:50 +0530

    Baseline implementation

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,19 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $result = [];
+
+        $handle = fopen($inputPath, 'r');
+        while (($line = fgets($handle)) !== false) {
+            [$url, $date] = explode(',', substr($line, 19), 2);
+            $date = substr($date, 0, 10);
+
+            $result[$url][$date] = ($result[$url][$date] ?? 0) + 1;
+        }
+        fclose($handle);
+
+        array_walk($result, fn(&$dates) => ksort($dates));
+
+        file_put_contents($outputPath, json_encode($result, JSON_PRETTY_PRINT));
     }
 }
